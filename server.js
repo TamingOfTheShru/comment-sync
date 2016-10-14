ff915d7d06aecaa06c757b860cfacae6e3b484f7
Scoot Developer <shruti@scootapp.com>
2016-10-14 18:40:57 +0530

    fetchData

--- server.js
+++ server.js
@@ -2,28 +2,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var fs = require('fs');
-var path    = require("path");
+var path = require("path");
 var movies = require('./routes/comments.route.js'); //routes are defined here
-var app = express(); 
+var app = express();
 var controller = require("./controllers/comments.controller.js");
+var Comments = require('mongoose').model('Comments');
 
 app.set('port', process.env.PORT || 8000);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('./'));
 
 mongoose.connect('mongodb://localhost/test');
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
 
-app.get('/',function(req,res){  
-     res.sendFile(path.join(__dirname+'/comment_form.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/comment_form.html'));
 });
 
-app.post('/saveData',function(req,res){  
-    controller.saveData(req,res)
+app.post('/saveData', function(req, res) {
+    controller.saveData(req, res)
 });
 
+app.get('/fetchData', function(req, res) {
+    Comments.findOne({ email: req.body.email }, function(err, doc) {
+        if (err) {
+        	console.log(err);
+        } else {
+        	var result = {
+        		"comment" : doc.comments,
+        		"diff" : doc.diff
+        	}
+        	res.send(result);
+        }
+    })
+})
